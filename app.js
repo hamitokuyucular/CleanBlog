0b46c447dc0be06e3cb01964a9ca6e26e1c93786
hamitokuyucular <hamitokuyucular@gmail.com>
2025-11-04 00:10:47 +0300

    app.js added

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const app = express();
+
+app.get("/", (req, res) => {
+
+  const blog = {
+    id: 1,
+    title: "Blog Title",
+    desc: "Blog description"
+  };
+
+  res.send(blog);
+
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Sunucu, ${port} portunda başlatıldı..`)
+})
