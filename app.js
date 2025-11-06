a215b578cf39740dedd50b2865e34997b916c353
hamitokuyucular <hamitokuyucular@gmail.com>
2025-11-06 16:55:14 +0300

    editing operations added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const ejs = require("ejs");
 const Post = require("./models/Post");
 
@@ -15,10 +16,13 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(methodOverride('_method', {
+  methods: ['POST', 'GET']
+}));
 
 // ROUTES
 app.get("/", async (req, res) => {
-  const posts = await Post.find();
+  const posts = await Post.find().sort("-dateCreated");
   res.render("index", {
     posts
   });
@@ -46,10 +50,34 @@ app.get("/add_post", (req, res) => {
 app.post("/posts", async (req, res) => {
   await Post.create(req.body);
   res.redirect("/")
+});
+
+app.get("/posts/edit_post/:id", async (req, res) => {
+  const post = await Post.findOne({_id: req.params.id});
+  res.render("edit_post", {
+    post
+  });
+});
+
+app.put("/posts/:id", async (req, res) => {
+  const post = await Post.findOne({_id: req.params.id});
+  post.authorName = req.body.authorName;
+  post.title = req.body.title;
+  post.detail = req.body.detail;
+  post.content = req.body.content;
+  post.save();
+
+  res.redirect(`/posts/${req.params.id}`)
+});
+
+app.delete("/posts/:id", async (req,res) => {
+  await Post.findByIdAndDelete(req.params.id);
+
+  res.redirect("/")
 })
 
 const port = 5000;
 
 app.listen(port, () => {
-  console.log(`Sunucu, ${port} portunda başlatıldı..`)
-})
+  console.log(`Sunucu, ${port} portunda başlatıldı..`);
+});
